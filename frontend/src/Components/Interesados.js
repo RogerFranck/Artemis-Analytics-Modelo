427349f03b0ae7ae8d5fd69f1895c1cafc66fcdb
sergio_mdza <sergio_mdza@outlook.com>
2020-04-17 00:12:05 -0500

    tabla interesado semi terminada - sin errores

--- frontend/src/Components/Interesados.js
+++ frontend/src/Components/Interesados.js
@@ -1,11 +1,74 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react';
+import MaterialTable from 'material-table';
+import axios from 'axios'
+import DeleteIcon from '@material-ui/icons/Delete';
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
 
-export default class Interesados extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Soy Interesados</h1>
-      </div>
-    )
-  }
+export default function MaterialTableDemo() {
+  const [columns, setColumns] = useState([
+   
+      { title: 'Nombre', field: 'nombre' },
+      { title: 'Correo', field: 'correo' },
+      { title: 'Numero', field: 'numero', type: 'numeric' },
+      { title: 'Cita', field: 'fechaCita' },
+    ]);
+
+    const [data, setData] = useState([
+    ]);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        const result = await axios('http://localhost:4000/prospectos/Interesados');
+        console.log(result)
+        setData(result.data);
+      };
+      fetchData();
+    }, []);
+  
+
+  return (
+    <MaterialTable
+      title="Editable Preview"
+      columns={columns}
+      data={data}
+      editable={{
+        onRowAdd: newData =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              {
+                const data = this.state.data;
+                data.push(newData);
+                this.setState({ data }, () => resolve());
+              }
+              resolve()
+            }, 1000)
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              {
+                const data = this.state.data;
+                const index = data.indexOf(oldData);
+                data[index] = newData;
+                this.setState({ data }, () => resolve());
+              }
+              resolve()
+            }, 1000)
+          }),
+        onRowDelete: oldData =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              {
+                let data = this.state.data;
+                const index = data.indexOf(oldData);
+                data.splice(index, 1);
+                this.setState({ data }, () => resolve());
+              }
+              resolve()
+            }, 1000)
+          }),
+      }}
+
+    />
+  )
 }
--- frontend/src/Components/contactados.js
+++ frontend/src/Components/contactados.js
@@ -4,7 +4,7 @@ export default class Contactados extends Component {
   render() {
     return (
       <div>
-        <h1> Soy contactado </h1>
+       <h1>Soy Contactado</h1> 
       </div>
     )
   }
