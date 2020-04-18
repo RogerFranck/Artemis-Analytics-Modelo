658440b4fe51c9cf36db90fa5cbe90a9ee39f91d
sergio_mdza <sergio_mdza@outlook.com>
2020-04-18 18:44:44 -0500

    Resolviendo error

--- frontend/src/Components/contactados.js
+++ frontend/src/Components/contactados.js
@@ -32,60 +32,29 @@ export default function MaterialTableDemo() {
 
   return (
     <MaterialTable
-      title="Editable Preview"
+      title="Action Overriding Preview"
       columns={columns}
       data={data}
-      editable={{
-        onRowAdd: newData =>
-          new Promise((resolve, reject) => {
-            setTimeout(() => {
-              {
-                const data = this.state.data;
-                data.push(newData);
-                this.setState({ data }, () => resolve());
-              }
-              resolve()
-            }, 1000)
-          }),
-        onRowUpdate: (newData, oldData) =>
-          new Promise((resolve, reject) => {
-            setTimeout(() => {
-              {
-                const data = this.state.data;
-                const index = data.indexOf(oldData);
-                data[index] = newData;
-                this.setState({ data }, () => resolve());
-              }
-              resolve()
-            }, 1000)
-          }),
-        onRowDelete: oldData =>
-          new Promise((resolve, reject) => {
-            setTimeout(() => {
-              {
-                let data = this.state.data;
-                const index = data.indexOf(oldData);
-                data.splice(index, 1);
-                this.setState({ data }, () => resolve());
-              }
-              resolve()
-            }, 1000)
-          }),
-      }}
-      options={{
-        actionsColumnIndex: -1
-      }}
+      actions={[
+        {
+          icon: 'save',
+          tooltip: 'Save User',
+          onClick: (event, rowData) => alert("You saved " + rowData.name)
+        },
+        {
+          icon: 'delete',
+          tooltip: 'Delete User',
+          onClick: (event, rowData) => alert("You want to delete " + rowData.name)
+        }
+      ]}
       components={{
         Action: props => (
-          <Button
-          //onClick={(event) => props.action.onClick(event, props.data)}
-          color="primary"
-          variant="contained"
-          style={{textTransform: 'none'}}
-          size="small"
-        >
-          My Button
-        </Button>
+          <Checkbox
+            //checked={state.checkedB}
+            //onChange={handleChange}
+            name="checkedB"
+            color="primary"
+          />
         ),
       }}
     />
